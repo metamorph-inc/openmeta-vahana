36b23cde8112371d22f5a9d19f3817c91b6391ab
Austin Tabulog <austin.tabulog@metamorphsoftware.com>
2017-06-05 16:02:24 -0500

    Cruise power has been completed.

--- scripts/cruise_power.py
+++ scripts/cruise_power.py
@@ -1,5 +1,5 @@
 """
-Attempt at translating Matlab to Python.
+Translating MatLab to Python 2.7
 
 built from A^3's CruisePower.m code
 """
@@ -11,141 +11,161 @@ import numpy as np
 import platform
 from subprocess import Popen, PIPE, STDOUT
 import os
+import math
 
 class CruisePower(Component):
 
     def __init__(self):
         super(CruisePower, self).__init__()
 
-        self.add_param('Vehicle', val=1.0, description='Vehicle type')	#arb.add_param('vehicle', val=Vehicle, description='would this be how you would get the values from vehicle from an file called arb?')
+        self.add_param('Vehicle', val=1.0, description='Vehicle type')	
         self.add_param('rProp', val=1.0, description='prop/rotor radius [m]')
         self.add_param('V', val=1.0, description='Cruise speed [m/s]')
         self.add_param('W', val=1.0, description='Weight of vehicle [N]')
 
         self.add_output('etaProp', val=1.0, description='efficiency of Prop')
         self.add_output('etaMotor', val=1.0, description='efficiency of motor')
-        self.add_output('VStall', val=35, description='Stall speed [m/s]')
-        self.add_output('CLmax', val=1.0, description='maximum CL for aircraft, section cl is much higher')
+        self.add_output('VStall', val=35, description='Stall speed [m/s]')      self.add_output('CLmax', val=1.0, description='maximum CL for aircraft, section cl is much higher')
         self.add_output('rho', val=1.0, description='density of air at cruise altitude. should be solved for using a database of altitude information')
 
-        self.add_output('bRef', val=1.0, description='reference wingspan assuming 2 props per wing with outboard props are at wingtips, 1 meter wide fuselage plus clearance between props and fuselage')
-        self.add_output('sRef', val=1.0, description='reference surface area of boht wings')
-        self.add_output('cRef', val=1.0, description='Chord calculation of each wing')
-        self.add_output('AR', val=1.0, description='')
+        self.add_output('b', val=1.0, description='reference wingspan assuming 2 props per wing with outboard props are at wingtips, 1 meter wide fuselage plus clearance between props and fuselage')
+        self.add_output('S', val=1.0, description='reference surface area of both wings')
+        self.add_output('c', val=1.0, description='Chord calculation of each wing')
+        self.add_output('AR', val=1.0, description='Aspect Ratio of wings')
         self.add_output('D', val=1.0, description='Force of Drag on vehicle at cruise')
         self.add_output('PCruise',val=1.0, description='Power required during cruise')
         self.add_output('PBattery', val=1.0, description='battery power required for cruise')
-        self.add_output('PBattery', val=1.0, description='battery power required for cruise')
-		
-		
-    def solve_nonlinear(self, params, unknowns, resids):
+        self.add_output('Cd0', val=1.0, description='Overall profile drag coefficent')
+		self.add_output('CL', val=1.0, description='CL during cruise')
+        self.add_output('PCruise', val=1.0, description='Power required to sustain Cruse')
+        self.add_output('PBattery', val=1.0, description='Power draw from battery')
+        self.add_output('LoverD', val=1.0, description='total lift over drag')
+        self.add_output('omega', val=1.0, description='angular velocity of blade tips')
+        self.add_output('alpha', val=1.0, description='inflow angle')
+        self.add_output('mu', val=1.0, description='advance ratio')
+        self.add_output('Ct', val=1.0, description='Thrust coefficient (including tip loss factor for effective disk area)')
+		self.add_output('lambda', val=1.0, description='induced velocity /w Newton method')
+        self.add_output('v', val=1.0, description='TBD')
+        self.add_output('SCdFuse', val=1.0, description='Drage area of fuselage and gears combined [m]')
+        self.add_output('VStall', val=1.0, description='stall velocity')
+        self.add_output('CLmax', val=1.0, description='CL at stall')
+        self.add_output('etaMotor', val=1.0, description='efficiency of motor')
+        self.add_output('Cd0Wing', val=1.0, description='wing profile drag coefficient')
+        self.add_output('e', val=1.0, description='span efficiency')
+        self.add_output('etaProp', val=1.0, description='prop efficiency')
+        self.add_output('B', val=1.0, description='Tip loss factor')
+        self.add_output('sigma', val=1.0, description='Blade solidity')
         
+        
+    def solve_nonlinear(self, params, unknowns, resids):
+       unknowns['SCdFuse'] = 0.35
+       
         if(params['vehicle'] == 0):
-            VStall = 35 # m/s
-            CLmax = 1.1 # Whole aircraft CL, section Clmax much higher
+            unknowns['VStall'] = 35 # m/s
+            unknowns['CLmax'] = 1.1 # Whole aircraft CL, section Clmax much higher
 
             # Compute Wingspan assuming 2 props per wing with outboard props are at
             # wingtips, 1 meter wide fuselage plus clearance between props and fuselage
-            unknowns['bRef'] = 6 * rProp + 1.2; # Rough distance between hubs of outermost props
+            unknowns['b'] = 6 * params['rProp'] + 1.2 # Rough distance between hubs of outermost props
 
             # Compute reference area (counting both wings)
-            SRef = W / (0.5 * rho * VStall^2 * CLmax);
+            unknowns['S'] = params['W'] / (0.5 * unknowns['rho'] * VStall^2 * CLmax)
 
             # Compute reference chord (chord of each wing)
-            cRef = 0.5 * SRef / bRef; 
+            unknowns['c'] = 0.5 * unknowns['S'] / unknowns['b'] 
                 
             # Equivalent aspect ratio
-            AR = bRef^2 / SRef;
+            unknowns['AR'] = unknowns['b']^2 / unknowns['S']
 
             # Motor efficiency
-            etaMotor = 0.85;
+            unknowns['etaMotor'] = 0.85
 
             # Wing profile drag coefficent
-            Cd0Wing = 0.012;
+            unknowns['Cd0Wing'] = 0.012
 
             # Overall profile drag
-            Cd0 = Cd0Wing + SCdFuse / SRef;
+            unknowns['Cd0'] = unknowns['Cd0Wing'] + unknowns['SCdFuse'] / unknowns['S']
 
             # Span efficiency
-            e = 1.3;
+            unknowns['e'] = 1.3
 
             # Solve for CL at cruise
-            CL = W / (0.5 * rho * V^2 * SRef);
+            unknowns['CL'] = params['W'] / (0.5 * unknowns['rho'] * params['V']^2 * unknowns['S'])
 
             # Prop efficiency
-            etaProp = 0.8;
+            unknowns['etaProp'] = 0.8
 
             # Estimate drag at cruise using quadratic drag polar
-            D = 0.5 * rho * V^2 * (SRef * (Cd0 + ...
-                CL^2 / (pi * AR * e)) + SCdFuse);
+            unknowns['D'] = 0.5 * unknowns['rho'] * params['V']^2 * (unknowns['S'] * (unknowns['Cd0'] + ...
+                unknowns['CL']^2 / (math.pi * unknowns['AR'] * unknowns['e'])) + unknowns['SCdFuse'])
 
             # Compute cruise power estimate
-            PCruise = D * V;
+            unknowns['PCruise'] = unknowns['D'] * params['V']
 
             # Battery power
-            PBattery = PCruise / etaProp / etaMotor;
+            unknowns['PBattery'] = unknowns['PCruise'] / unknowns['etaProp'] / unknowns['etaMotor']
 
             # Cruise L/D
-            LoverD = W / D;
+            unknowns['LoverD'] = params['W'] / unknowns['D']
 
         elif (params['vehicle'] == 1):
 
             # Motor efficiency
-            unknowns['etaMotor'] = 0.85 * 0.98; # Assumed motor and gearbox efficiencies (85%, and 98% respectively)
+            unknowns['etaMotor'] = 0.85 * 0.98 # Assumed motor and gearbox efficiencies (85%, and 98% respectively)
 
             # Tip Mach number constraint
-            MTip = 0.65;
+            MTip = 0.65
 
             # Tip loss factor 
-            B = 0.97;
+            unknowns['B'] = 0.97
 
             # Blade solidity
-            sigma = 0.1;
+            unknowns['sigma'] = 0.1
 
             # Blade profile drag coefficient
-            Cd0 = 0.012;
+            Cd0 = 0.012
 
             # Compute rotation rate at cruise to be at tip mach limit
-            omega = (340.2940 * MTip - V) / rProp;
+            unknowns['omega'] = (340.2940 * MTip - params['V']) / params['rProp']
 
             # Fuselage drag
-            D = 0.5 * rho * V^2 * SCdFuse;
+            unknowns['D'] = 0.5 * unknowns['rho'] * params['V']^2 * SCdFuse
 
             # Inflow angle 
-            alpha = atan(D/W);
+            unknowns['alpha'] = math.atan2(unknowns['D'], params['W'])
 
             # Compute advance ratio
-            mu = V * cos(alpha) / (omega * rProp);
+            unknowns['mu'] = params['V'] * math.cos(unknowns['alpha']) / (unknowns['omega'] * params['rProp'])
                 
             # Thrust coefficient (including tip loss factor for effective disk area)
-            Ct = W / (rho * pi * rProp^2 * B^2 * omega^2 * rProp^2);
+            unknowns['Ct'] = params['W'] / (unknowns['rho'] * ,math.pi * params['rProp']^2 * B^2 * unknowns['omega']^2 * params['rProp']^2)
 
             # Solve for induced velocity /w Newton method (see "Helicopter Theory" section 4.1.1)
-            lambda = mu * tan(alpha) + Ct / ...
-                (2 * sqrt(mu^2 + Ct/2));
+            unknowns['lambda'] = unknowns['mu'] * math.tan(alpha) + unknowns['Ct'] / ...
+                (2 * math.sqrt(unknowns['mu']^2 + unknowns['Ct']/2))
             for i = 1:5
-                lambda  = (mu * tan(alpha) + ...
-                    Ct / 2 * (mu^2 + 2*lambda^2) / ...
-                    (mu^2 + lambda^2)^1.5) / ...
-                    (1 + Ct/2 * lambda / (mu^2 + lambda^2)^1.5);
+                unknowns['lambda']  = (unknowns['mu'] * math.tan(unknowns['alpha']) + ...
+                    unknowns['Ct'] / 2 * (unknowns['mu']^2 + 2*unknowns['lambda']^2) / ...
+                    (unknowns['mu']^2 + unknowns['lambda']^2)^1.5) / ...
+                    (1 + unknowns['Ct']/2 * unknowns['lambda'] / (unknowns['mu']^2 + unknowns['lambda']^2)^1.5)
             end
-            v = lambda * omega * rProp - V * sin(alpha);
+            unknowns['v'] = unknowns['lambda'] * unknowns['omega'] * params['rProp'] - params['V'] * math.sin(unknowns['alpha'])
 
             # Power in forward flight (see "Helicopter Theory" section 5-12)
-            PCruise = W * (V * sin(alpha) + 1.3 * cosh(8 * mu^2) * v + ...
-                Cd0 * omega * rProp * ...
-                (1 + 4.5 * mu^2 + 1.61 * mu^3.7) * ...
-                (1 - (0.03 + 0.1 * mu + 0.05 * sin(4.304 * mu - 0.20)) * ...
-                (1-cos(alpha)^2)) / 8 / (Ct / sigma));
+            unknowns['PCruise'] = params['W'] * (params['V'] * math.sin(unknowns['alpha']) + 1.3 * math.cosh(8 * unknowns['mu']^2) * unknowns['v'] + ...
+                unknowns['Cd0'] * unknowns['omega'] * params['rProp'] * ...
+                (1 + 4.5 * unknowns['mu']^2 + 1.61 * unknowns['mu']^3.7) * ...
+                (1 - (0.03 + 0.1 * unknowns['mu'] + 0.05 * math.sin(4.304 * unknowns['mu'] - 0.20)) * ...
+                (1-math.cos(unknowns['alpha'])^2)) / 8 / (unknowns['Ct'] / unknowns['sigma']))
 
             # 10% power added for helicopter tail rotor
-            PCruise = 1.1 * PCruise;
+            unknowns['PCruise'] = 1.1 * unknowns['PCruise']
 
             # Equivalent L/D, assuming power = D * V and L = W
-            LoverD = W / (PCruise / V);
+            unknowns['LoverD'] = params['W'] / (unknowns['PCruise'] / params['V'])
 
             # Battery power
-            PBattery = PCruise / etaMotor;
+            unknowns['PBattery'] = unknowns['PCruise'] / etaMotor
 
             else
 
