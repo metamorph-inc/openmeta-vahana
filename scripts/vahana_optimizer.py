bd6883bc2da15c7df9ac70bd3d0d7260b9606f81
Joseph Coombe <jcoombe@metamorphsoftware.com>
2017-06-15 16:35:51 -0500

    Test run using dummy TestSystem Component

--- scripts/vahana_optimizer.py
+++ scripts/vahana_optimizer.py
@@ -24,16 +24,45 @@ from __future__ import print_function  # allows for backwards compatibility with
 import sys
 import math
 
-from openmdao.api import Problem, Group, Component, IndepVarComp, ExecComp, \  # OpenMDAO imports
+from openmdao.api import Problem, Group, Component, IndepVarComp, ExecComp, \
                          ScipyOptimizer, SubProblem, FullFactorialDriver, \
-                         SqliteRecorder
+                         SqliteRecorder  # OpenMDAO imports
 
-import sqlitedict
+import sqlitedict  # recorder import
 from pprint import pprint
 
 #TODO: Add Component imports
 #      and(or)
 #      Add Component definitions
+class TestSystem(Component):
+    ''' costPerFlight = range*rProp - cruiseSpeed*batteryMass - (motorMass*mtom)^3
+        - (range*cruiseSpeed)^2 + rProp*motorMass - cruiseSpeed*mtom '''
+    def __init__(self):
+        super(TestSystem, self).__init__()
+        
+        self.add_param('range', val=50.0)
+        self.add_param('rProp', val=1.0)
+        self.add_param('cruiseSpeed', val=50.0)
+        self.add_param('batteryMass', val=117.0)
+        self.add_param('motorMass', val=30.0)
+        self.add_param('mtom', val=650.0)
+        
+        self.add_output('costPerFlight', shape=1)
+        
+    def solve_nonlinear(self, params, unknowns, resids):
+        ''' costPerFlight = range*rProp - cruiseSpeed*batteryMass - (motorMass*mtom)^3
+            - (range*cruiseSpeed)^2 + rProp*motorMass - cruiseSpeed*mtom '''
+        
+        range = params['range']
+        rProp = params['rProp']
+        cruiseSpeed = params['cruiseSpeed']
+        batteryMass = params['batteryMass']
+        motorMass = params['motorMass']
+        mtom = params['mtom']
+        
+        unknowns['costPerFlight'] = range*rProp - cruiseSpeed*batteryMass - (motorMass*mtom)**3 \
+                                    - (range*cruiseSpeed)**2 + rProp*motorMass - cruiseSpeed*mtom
+        
 
 #TODO: Add Group imports
 #      and(or)
@@ -45,20 +74,29 @@ class TopLevelSystem(Group):
         super(TopLevelSystem, self).__init__()
         
         # add independent variables - IS THIS EVEN NEEDED?
-        # E.g: self.add('indep1', IndepVarComp('x', 0.0))
-        #TODO
+        self.add('indep1', IndepVarComp('range', 50.0))
+        self.add('indep2', IndepVarComp('rProp', 1.0))
+        self.add('indep3', IndepVarComp('cruiseSpeed', 50.0))
+        self.add('indep4', IndepVarComp('batteryMass', 117.0))
+        self.add('indep5', IndepVarComp('motorMass', 30.0))
+        self.add('indep6', IndepVarComp('mtom', 650.0))
         
         # add components
-        # E.g: self.add('comp', System())
-        #TODO
+        self.add('comp', TestSystem())
         
         # add constraint equations
-        # E.g: self.add('con, ExecComp('c = x-y))
-        #TODO
+        self.add('con', ExecComp('c = motorMass + batteryMass - mtom'))
         
-        # connect components and variables
-        # E.g: self.connect('indep1.x', 'comp.x') 
-        #TODO        
+        # connect components and variablesW
+        self.connect('indep1.range', 'comp.range') 
+        self.connect('indep2.rProp', 'comp.rProp') 
+        self.connect('indep3.cruiseSpeed', 'comp.cruiseSpeed') 
+        self.connect('indep4.batteryMass', 'comp.batteryMass')
+        self.connect('indep5.motorMass', 'comp.motorMass')
+        self.connect('indep6.mtom', 'comp.mtom')
+        self.connect('comp.motorMass', 'con.motorMass')
+        self.connect('comp.batteryMass', 'con.batteryMass')
+        self.connect('comp.mtom', 'con.mtom')
         
         
 if __name__ == '__main__':
@@ -67,61 +105,72 @@ if __name__ == '__main__':
     
     # SubProblem: set up the optimizer
     sub.driver = ScipyOptimizer()
-    sub.driver.options['optimizer'] = 'COBYLA'
-    sub.driver.options['disp'] = True  # disable optimizer output
+    sub.driver.options['optimizer'] = 'COBYLA'  # The 'COBYLA' optimizer is supported by OpenMETA. 
+                                                # Unlike the 'SLSQP' optimizer, the 'COBYLA' optimizer doesn't require a Jacobian matrix.
+    sub.driver.options['disp'] = True  # enable optimizer output
     
     # SubProblem: set design variables for sub.driver
-    # E.g: sub.driver.add_desvar("indep1.x", lower=3.1, upper=50)
-    #TODO    
+    sub.driver.add_desvar('indep2.rProp', lower=0.3, upper=2.0)
+    sub.driver.add_desvar('indep3.cruiseSpeed', lower=39.0, upper=80.0)
+    sub.driver.add_desvar('indep4.batteryMass', lower=10.0, upper=999.0)
+    sub.driver.add_desvar('indep5.motorMass', lower=1.0, upper=999.0)
+    sub.driver.add_desvar('indep6.mtom', lower=100.0, upper=9999.0)
     
     # SubProblem: set design objectives
-    # sub.driver.add_objective("comp.f_xyz")
-    #TODO
+    sub.driver.add_objective('comp.costPerFlight')
     
     # SubProblem: set constraints
-    # E.g: sub.driver.add_constraint("con.c", lower=15.0)
-    #TODO
+    sub.driver.add_constraint("con.c", lower=0.0)
     
     # TopProblem: define a Problem to set up different optimization cases
     top = Problem(root=Group())
     
     # TopProblem: add independent variables
-    # E.g: prob.root.add("top_indep1", IndepVarComp('x', 3.1))
-    #TODO
+    #top.root.add('vehicle', IndepVarComp('vehicle'))  # 1st get this working with just the tiltwing
+    top.root.add('indep1', IndepVarComp('range', 50.0))
+    top.root.add('indep2', IndepVarComp('rProp', 1.0))
+    top.root.add('indep3', IndepVarComp('cruiseSpeed', 50.0))
+    top.root.add('indep4', IndepVarComp('batteryMass', 117.0))
+    top.root.add('indep5', IndepVarComp('motorMass', 30.0))
+    top.root.add('indep6', IndepVarComp('mtom', 650.0))
     
     # TopProblem: add the SubProblem
-    # E.g: prob.root.add("subprob", SubProblem(sub, params=['indep1.x', 'indep2.y', 'indep3.z'],
-    #      unknowns=['comp.f_xyz']))
-    #TODO
+    top.root.add('subprob', SubProblem(sub, params=['indep1.range', 'indep2.rProp', \
+                                                    'indep3.cruiseSpeed', 'indep4.batteryMass', \
+                                                    'indep5.motorMass', 'indep6.mtom'],
+                                            unknowns=['comp.costPerFlight']))
     
     # TopProblem: connect top's independent variables to sub's params
-    # E.g: prob.root.connect("top_indep1.x", "subprob.indep1.x")
-    #TODO
+    top.root.connect('indep1.range', 'subprob.indep1.range')
+    top.root.connect('indep2.rProp', 'subprob.indep2.rProp')
+    top.root.connect('indep3.cruiseSpeed', 'subprob.indep3.cruiseSpeed')
+    top.root.connect('indep4.batteryMass', 'subprob.indep4.batteryMass')
+    top.root.connect('indep5.motorMass', 'subprob.indep5.motorMass')
+    top.root.connect('indep6.mtom', 'subprob.indep6.mtom')
     
     # TopProblem: set up the parameter study
     # for a parameter study, the following drivers can be used:
     # UniformDriver, FullFactorialDriver, LatinHypercubeDriver, OptimizedLatinHypercubeDriver
     # in this case, we will use FullFactorialDriver
-    prob.driver = FullFactorialDriver(num_levels=20)
+    top.driver = FullFactorialDriver(num_levels=21)
     
     # TopProblem: add top.driver's design variables
-    # E.g: prob.driver.add_desvar('top_indep3.z', lower=0.0, upper=20.0)
-    #TODO
+    top.driver.add_desvar('indep1.range', lower=10.0, upper=20.0)  #TODO: Change lower bounds to '10000.0' and upper bound to '200000.0'
     
     # Data collection
     recorder = SqliteRecorder('subprob')
     recorder.options['record_params'] = True
     recorder.options['record_metadata'] = True
-    prob.driver.add_recorder(recorder)
+    top.driver.add_recorder(recorder)
     
     # Setup
-    prob.setup(check=False)
+    top.setup(check=False)
     
     # Run 
-    prob.run()
+    top.run()
     
     # Cleanup
-    prob.cleanup()
+    top.cleanup()
     
     # Data retrieval & display
     db = sqlitedict.SqliteDict( 'subprob', 'iterations' )
