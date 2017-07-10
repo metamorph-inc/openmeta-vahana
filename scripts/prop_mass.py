dc44157f1cd447747052685e21f848648a3a9482
Joseph Coombe <jcoombe@metamorphsoftware.com>
2017-07-10 17:42:23 -0500

    Trying to figure out why the OpenMETA optimizer seems to produce different results than the OpenMDAO optimizer

---
+++ scripts/prop_mass.py
@@ -0,0 +1,21 @@
+'''
+# Name: wing_mass.py
+# Company: MetaMorph, Inc.
+# Author(s): Joseph Coombe
+# Email: jcoombe@metamorphsoftware.com
+# Create Date: 6/29/2017
+# Edit Date: 6/29/2017
+
+# Conversion of Airbus A^3's vahanaTradeStudy>reserveMission.mat code
+# (located here: https://github.com/VahanaOpenSource/vahanaTradeStudy ) 
+# to Python 2.7 for use in the MetaMorph, Inc. OpenMETA environment.
+
+# Estimate propeller blade mass s
+
+# Inputs:
+#   R - rotor radius [m]
+#   T - maximum thrust [N]
+
+# Outputs:
+#   mass - Mass of the blades for one propeller [kg]
+'''
