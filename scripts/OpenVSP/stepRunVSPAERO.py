8ba70a7da957c1d08f077cb8d93f9e03256b46f3
Joseph Coombe <jcoombe@metamorphsoftware.com>
2017-09-22 08:42:35 -0500

    Add draft of python script to run VSPAERO in an OpenVSP testbench

--- scripts/OpenVSP/stepRunVSPAERO.py
+++ scripts/OpenVSP/stepRunVSPAERO.py
@@ -14,4 +14,52 @@
 # Written for OpenVSP Version-3.13.2-win32.
 '''
 
+import json
+import os.path
+import posixpath
+import platform
+import sys
 
+def read_testbench_manifest_parameters(testbench_manifest):
+    parameters_dict = dict()
+    print "Reading parameters from testbench_manifest.json..."
+    print
+    for parameter in testbench_manifest['Parameters']:
+        parameters_dict[parameter['Name']] = parameter['Value']
+        print parameter['Name'] + ": " + str(parameter['Value'])
+    print
+    return parameters_dict
+
+if __name__ == '__main__':
+    print "Running " + str(__file__) + "..."
+
+    # Obtain testbench configuration
+    with open('testbench_manifest.json', 'r') as f_in:
+        testbench_manifest = json.load(f_in)
+        
+    replacement_dict = read_testbench_manifest_parameters(testbench_manifest)
+    vspFilename = replacement_dict.get('VSP_Filename')
+    if vspFilename is None:
+        raise ValueError("OpenVSP filename must be provided - e.g. 'vspmodel.vsp3'")
+    vspName = vspFilename[2:-6]  # Trim off "u' and .vsp3" from ends of filename
+
+    # Run vspaero.exe
+    print "Running vspaero.exe"
+    
+    fileDir = os.path.dirname(os.path.realpath(__file__))
+    exeName = ''
+    if platform.system() == 'Windows':
+        exeName = 'vspaero.exe'
+    elif platform.system() == 'Darwin':
+        exeName = 'vspaero'
+    path = os.path.join(fileDir, exeName)
+    print "Path: " + path
+    
+    
+    
+    
+    
+    
+    
+    
+    
